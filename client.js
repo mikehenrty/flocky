a05aa927f7f13dd208a7012c3c58f76f2bd7bef5
Michael Henretty <michael.henretty@gmail.com>
2016-03-15 20:42:10 +0100

    add web client

---
+++ client.js
@@ -0,0 +1,32 @@
+const SERVER_URL = 'ws://flocky.local:8001';
+
+var output = document.getElementById('output');
+function write(message) {
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+}
+
+write('starting');
+var ws = new WebSocket(SERVER_URL);
+
+ws.onopen = function(evt) {
+  write('opened connection');
+}
+
+ws.onclose = function(evt) {
+  write('closed');
+};
+
+ws.onerror = function(evt) {
+  write('error ' + evt);
+};
+
+ws.onmessage = function(evt) {
+  if (evt.data == '1') {
+    document.body.style.backgroundColor = 'blue';
+  } else {
+    document.body.style.backgroundColor = 'red';
+  }
+};
